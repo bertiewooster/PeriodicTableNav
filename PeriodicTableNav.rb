f62e6da8e9cc1102e003a70c4a2a23d8cf0a0b60
Jeremy Monat <jmonat@whitegloveapps.com>
2013-12-01 14:57:15 -0500

    Breaks out elements by period on home page (periodic table)

--- PeriodicTableNav.rb
+++ PeriodicTableNav.rb
@@ -9,18 +9,35 @@ DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/Elements.sqlite")
       
 class Element  
 	include DataMapper::Resource  
-	property :atomicnum, Integer, :key => true
+	property :atomic_num, Integer, :key => true
 	property :symbol, Text
 	property :name, Text
-	property :econfigvalence, Text
+	property :e_config_valence, Text
 	property :period, Integer
 	property :group, Integer
+end  
+
+class EConfigBase  
+	include DataMapper::Resource  
+	property :id, Integer, :key => true
+	property :base, Text
 end  
 
 DataMapper.finalize.auto_upgrade!
 
 get '/' do  # load home page
-	@elements = Element.all #:order => :atomicnum.desc  
-	@title = 'All Elements'  
+	@elements			= Element.all #:order => :atomicnum.desc
+	@max_period			= Element.last.period
+	
+	@ebyp = Array.new(@max_period+1, "things")
+	@ebyp.each_index do |period|
+		@ebyp[period] = @elements.select{|element| element.period == period}
+	end
+		
+	@elementlast		= Element.last
+	@elementp1			= @elements.select{|element| element.period == 1}
+	@econfigbases		= EConfigBase.all
+	@econfigbaselast	= EConfigBase.last
+	@title = 'All Elements'
 	erb :home  # template: home page 
 end  
